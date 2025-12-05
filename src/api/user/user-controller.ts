5f04874ae6bfa03076e14ba233576e404cbeda7c
Hector Valenzuela <francisco1495@gmail.com>
2025-12-04 17:51:52 -0700

    progress

--- src/api/user/user-controller.ts
+++ src/api/user/user-controller.ts
@@ -2,7 +2,7 @@ import user_model from './user-model'
 import type { Request, Response } from 'express'
 import { error_response } from '../../utils/error-response'
 import { async_handler } from '../../utils/async_handler'
-import { authenticated_request } from '../../types/authenticated'
+import { authenticated_request, authenticated_user } from '../../types/authenticated'
 
 export const create_user = async_handler(async (req: Request, res: Response) => {
   const { first_name, last_name, email, password } = req.body
@@ -20,7 +20,16 @@ export const create_user = async_handler(async (req: Request, res: Response) => 
 })
 
 export const delete_user = async_handler(async(req: Request, res: Response) => {
-  
+  const { userId} = (req as authenticated_request).user
+
+  const user = await user_model.findByIdAndDelete(userId)
+
+  if (!user) throw new error_response('User does not exist', 404)
+
+  res.status(200).json({
+    success: true,
+    message: `User deleted successfully ${user.first_name} ${user.last_name}}`
+  })
 })
 
 export const get_user_info = async_handler(async(req: Request, res: Response) => {
